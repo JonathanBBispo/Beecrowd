5a485356d0025616fa3b9c6b47f5338a93c5f659
Jonathan Bispo <jokz.bis@gmail.com>
2025-04-11 20:34:09 -0300

    learn(c-1007): resolve exercicio diference

---
+++ C/bee1007.c
@@ -0,0 +1,10 @@
+//Diference
+#include <stdio.h>
+
+int main(){
+    int a,b,c,d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int dif = (a*b) - (c*d);
+    printf("DIFERENCA = %d\n", dif);
+    return 0;
+}
