635a4152b2d6c24d4899cfb40bd16efb5d80e37d
Jonathan Bispo <jokz.bis@gmail.com>
2025-07-28 20:21:13 -0300

    learn(c-1011): resolve exercicio esfera

---
+++ C/bee1011.c
@@ -0,0 +1,12 @@
+//Esfera
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159
+
+int main(){
+    int r;
+    scanf("%d", &r);
+    double volume = (4.0/3) * PI * pow(r, 3);
+    printf("VOLUME = %.3f\n", volume);
+}
