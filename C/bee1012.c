6a024a76950a71e70c2e1cc5b24fbe8ad08b3a5d
Jonathan Bispo <jokz.bis@gmail.com>
2025-07-28 21:11:39 -0300

    learn(c-1012): resolve exercicio area

---
+++ C/bee1012.c
@@ -0,0 +1,14 @@
+//Area
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14159
+
+int main(){
+    double a, b, c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    printf("TRIANGULO: %.3f\n", (a * c)/2);
+    printf("CIRCULO: %.3f\n", PI * pow(c,2));
+    printf("TRAPEZIO: %.3f\n", (a + b) * c /2);
+    printf("QUADRADO: %.3f\n", pow(b,2));
+    printf("RETANGULO: %.3f\n", a*b);
+}
